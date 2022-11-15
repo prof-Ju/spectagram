d17da223eb1902596c0cf1896666cada9ef6a08a
Prof Ju & Alunos <90655877+prof-Ju@users.noreply.github.com>
2022-11-15 15:26:30 -0300

    Update createPost.js

--- createPost.js
+++ createPost.js
@@ -1,3 +1,41 @@
+<View style={styles.container}>
+                <SafeAreaView style={styles.droidSafeArea} />
+                <View style={styles.appTitle}>
+                    <View style={styles.appIcon}>
+                        <Image
+                            source={require("../assets/logo.png")}
+                            style={styles.iconImage}
+                        ></Image>
+                    </View>
+                    <View style={styles.appTitleTextContainer}>
+                        <Text style={styles.appTitleText}>Novo Post</Text>
+                    </View>
+                </View>
+                <View style={styles.fieldsContainer}>
+                    <ScrollView>
+                        <Image
+                            source={preview_images[this.state.previewImage]}
+                            style={styles.previewImage}
+                        ></Image>
+                        <View style={{ height: RFValue(this.state.dropdownHeight) }}>
+                            
+                            
+                            // INSIRA O DROPDOWN AQUI!!
+                            
+                            
+                                                  </View>
+
+                        <TextInput
+                            style={styles.inputFont}
+                            onChangeText={caption => this.setState({ caption })}
+                            placeholder={"Legenda"}
+                            placeholderTextColor="white"
+                        />
+                    </ScrollView>
+                </View>
+                <View style={{ flex: 0.08 }} />
+            </View>
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
